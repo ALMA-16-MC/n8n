af6f0064752bbea064a01dc5fe741af736b55ca7
ALMA-16-MC <159382313+ALMA-16-MC@users.noreply.github.com>
2026-04-01 16:31:16 -0600

    Update form trigger test to use new node type

--- cypress/e2e/16-form-trigger-node.cy.ts
+++ cypress/e2e/16-form-trigger-node.cy.ts
@@ -12,85 +12,65 @@ describe('n8n Form Trigger', () => {
 	it("add node by clicking on 'On form submission'", () => {
 		workflowPage.getters.canvasPlusButton().click();
 		workflowPage.getters.nodeCreatorNodeItems().contains('On form submission').click();
+
 		ndv.getters.parameterInput('formTitle').type('Test Form');
 		ndv.getters.parameterInput('formDescription').type('Test Form Description');
+
 		ndv.getters.backToCanvas().click();
 		workflowPage.getters.nodeIssuesByName('On form submission').should('not.exist');
 	});
 
 	it('should fill up form fields', () => {
-		workflowPage.actions.addInitialNodeToCanvas('n8n Form Trigger', {
+		workflowPage.actions.addInitialNodeToCanvas('On form submission', {
 			isTrigger: true,
 			action: 'On new n8n Form event',
 		});
+
 		ndv.getters.parameterInput('formTitle').type('Test Form');
 		ndv.getters.parameterInput('formDescription').type('Test Form Description');
+
+		// Add first field
 		cy.get('[data-test-id="fixed-collection-add"]').click();
 		ndv.getters.parameterInput('fieldLabel').type('Test Field 1');
 		ndv.getters.parameterInput('fieldType').click();
-		//fill up first field of type number
 		getVisibleSelect().contains('Number').click();
-		cy.get(
-			'[data-test-id="parameter-input-requiredField"] > .parameter-input > .el-switch > .el-switch__core',
-		).click();
-		//fill up second field of type text
-		cy.get('.fixed-collection-parameter > :nth-child(2) > .button > span').click();
-		cy.get('.border-top-dashed > .parameter-input-list-wrapper > :nth-child(1) > .parameter-item')
-			.find('input[placeholder*="e.g. What is your name?"]')
-			.type('Test Field 2');
-		//fill up second field of type date
-		cy.get('.fixed-collection-parameter > :nth-child(2) > .button > span').click();
-		cy.get(
-			':nth-child(3) > .border-top-dashed > .parameter-input-list-wrapper > :nth-child(1) > .parameter-item',
-		)
-			.find('input[placeholder*="e.g. What is your name?"]')
-			.type('Test Field 3');
-		cy.get(
-			':nth-child(3) > .border-top-dashed > .parameter-input-list-wrapper > :nth-child(2) > .parameter-item',
-		).click();
-		getVisibleSelect().contains('Date').click();
-		// fill up second field of type dropdown
-		cy.get('.fixed-collection-parameter > :nth-child(2) > .button').click();
-		cy.get(
-			':nth-child(4) > .border-top-dashed > .parameter-input-list-wrapper > :nth-child(1) > .parameter-item',
-		)
-			.find('input[placeholder*="e.g. What is your name?"]')
-			.type('Test Field 4');
-		cy.get(
-			':nth-child(4) > .border-top-dashed > .parameter-input-list-wrapper > :nth-child(2) > .parameter-item',
-		).click();
-		getVisibleSelect().contains('Dropdown').click();
-		cy.contains('button', 'Add Field Option').click();
-		cy.contains('label', 'Field Options')
-			.parent()
-			.parent()
-			.nextAll()
-			.find('[data-test-id="parameter-input-field"]')
-			.eq(0)
-			.type('Option 1');
-		cy.contains('label', 'Field Options')
-			.parent()
-			.parent()
-			.nextAll()
-			.find('[data-test-id="parameter-input-field"]')
-			.eq(1)
-			.type('Option 2');
 
-		//add optional submitted message
+		cy.get(
+			'[data-test-id="parameter-input-requiredField"] .el-switch__core',
+		).click();
+
+		// Add second field (Text)
+		cy.contains('button', 'Add Field').click();
+		cy.get('input[placeholder*="What is your name?"]').eq(0).type('Test Field 2');
+
+		// Add third field (Date)
+		cy.contains('button', 'Add Field').click();
+		cy.get('input[placeholder*="What is your name?"]').eq(1).type('Test Field 3');
+
+		cy.get('[data-test-id="parameter-input-fieldType"]').eq(1).click();
+		getVisibleSelect().contains('Date').click();
+
+		// Add fourth field (Dropdown)
+		cy.contains('button', 'Add Field').click();
+		cy.get('input[placeholder*="What is your name?"]').eq(2).type('Test Field 4');
+
+		cy.get('[data-test-id="parameter-input-fieldType"]').eq(2).click();
+		getVisibleSelect().contains('Dropdown').click();
+
+		cy.contains('button', 'Add Field Option').click();
+
+		cy.get('[data-test-id="parameter-input-field"]').eq(0).type('Option 1');
+		cy.get('[data-test-id="parameter-input-field"]').eq(1).type('Option 2');
+
+		// Add optional response message
 		cy.get('.param-options').click();
-		getVisibleSelect().find('span').contains('Form Response').click();
+		getVisibleSelect().contains('Form Response').click();
+
 		cy.contains('span', 'Text to Show')
-			.should('exist')
 			.parent()
 			.parent()
 			.next()
-			.children()
-			.children()
-			.children()
-			.children()
-			.children()
-			.children()
-			.children()
+			.find('input, textarea')
 			.first()
 			.type('Your test form was successfully submitted');
 
